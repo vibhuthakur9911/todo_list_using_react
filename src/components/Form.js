63a764552cae2d110cd6317b9b434eec31cb3cff
Vaibhav <varunrajput12o8@gmail.com>
2023-08-03 14:36:44 +0530

    Todo First Commit

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+function Form(props) {
+  const [name, setName] = useState('');
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    props.addTask(name);
+    setName("");
+  }
+
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className=" my-label">
+          What needs to be done?
+        </label>
+      </h2>
+
+     <div className="from-input">
+     <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg my-input"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg my_btn">
+        Add
+      </button>
+     </div>
+    </form>
+  );
+}
+
+export default Form;
